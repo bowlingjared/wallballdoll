96821659d6c2de2c414932b4225fa898b6317df8
Jared Bowling <jsbowling1@outlook.com>
2026-08-11 21:20:44 -0500

    Fix build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:onnxruntime_v2/onnxruntime_v2.dart';
 import 'package:wallball/main.dart';
 
 void main() {
